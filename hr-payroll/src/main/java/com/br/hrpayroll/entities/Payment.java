a459d86b56f934060fab8208cac54446f017de11
Marcelo <marcelojunin2010@hotmail.com>
2021-03-04 20:50:43 -0300

    hr-payroll implementarion mock

---
+++ hr-payroll/src/main/java/com/br/hrpayroll/entities/Payment.java
@@ -0,0 +1,45 @@
+package com.br.hrpayroll.entities;
+
+public class Payment {
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public Payment() {
+    }
+
+    public Payment(final String name, final Double dailyIncome, final Integer days) {
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+        this.days = days;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(final Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(final Integer days) {
+        this.days = days;
+    }
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+}
---
+++ hr-payroll/src/main/java/com/br/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.br.hrpayroll.services;
+
+import com.br.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("bob", 200.0, days);
+    }
+}
