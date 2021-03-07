f5ba56bd1183be03727fdfaf87438c4ae5c4a6b8
Marcelo <marcelojunin2010@hotmail.com>
2021-03-07 16:14:07 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/br/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/br/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.br.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,13 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    /**
+     * O test.config vem do github, do projeto de configuracao.
+     * Ele é espelhado no bootstrap.yml, em cloud.config.uri
+     */
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
 
@@ -30,6 +38,12 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<List<Worker>> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping(value = "/{id}")
